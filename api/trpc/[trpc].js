a29da172dd3a9e218de96b0b1d114c23ad2c3d6b
SEUB66 <129697590+SEUB66@users.noreply.github.com>
2025-12-25 20:46:49 -0500

    Fix: Import appRouter directly from routers to reduce bundle size

--- api/trpc/[trpc].js
+++ api/trpc/[trpc].js
@@ -1,5 +1,5 @@
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
-import { appRouter } from '../../dist/index.js';
+import { appRouter } from '../../server/routers.js';
 
 export default async function handler(request) {
   return fetchRequestHandler({
