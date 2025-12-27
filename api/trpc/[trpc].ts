7eede464a21d77d7e02fdc7d4d12a5639b66354d
SEUB66 <129697590+SEUB66@users.noreply.github.com>
2025-12-26 20:08:18 -0500

    Fix: Add .js extension to ES module import in Vercel API route

--- api/trpc/[trpc].ts
+++ api/trpc/[trpc].ts
@@ -1,5 +1,5 @@
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
-import { appRouter } from '../../server/routers';
+import { appRouter } from '../../server/routers.js';
 
 export default async function handler(request: Request) {
   try {
